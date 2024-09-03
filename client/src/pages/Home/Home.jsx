b381214545da8f8a9108ee76f8f7f680ee84be6a
MirArfan <mir.arfan.iiuc9@gmail.com>
2024-09-03 15:30:17 +0600

    first commit

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu/>
+    </div>
+  )
+}
+
+export default Home
